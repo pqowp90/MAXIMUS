42f51a6e3a0eb7f179a475b29b653498e24d9f25
SimGyuYeong <shgy5901@gmail.com>
2022-12-21 23:56:59 +0900

    Sound Setting

--- Assets/01. Scripts/SliderSound.cs
+++ Assets/01. Scripts/SliderSound.cs
@@ -12,14 +12,16 @@ public class SliderSound : MonoBehaviour
 
     [SerializeField]
     private TMP_Text valueText,nameText;
+
     private void Start(){
         slider = GetComponentInChildren<Slider>();
         audioMixer = MenuUi.Instance.audioMixer;
         ValueChanged(PlayerPrefs.GetFloat(nameText.text, 0f));
         slider.value = PlayerPrefs.GetFloat(nameText.text, 0f);
     }
+
     public void ValueChanged(float value){
-        valueText.text = string.Format("{0}",(int)(100-(-value)/4*10f));
+        valueText.text = string.Format("{0}%",(int)(100-(-value)/4*10f));
         
         if(value == -40f) audioMixer.SetFloat(nameText.text,-80);
         else audioMixer.SetFloat(nameText.text, value);
